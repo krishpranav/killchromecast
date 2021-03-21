133a6adc5d5eda469efce4a4f5705cc18e4634b3
krishpranav <krisna.pranav@gmail.com>
2021-03-21 17:55:40 +0530

    done changes

---
+++ killcast.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#imports
+import os
+import sys
+import json
+import time
+import socket
+import requests
+import argparse
+import ipaddress
+from xml.etree import ElementTree
