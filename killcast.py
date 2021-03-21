ed1354705ff13ff8b15f13ab9a68b65941573990
krishpranav <krisna.pranav@gmail.com>
2021-03-21 17:56:25 +0530

    adding colors

--- killcast.py
+++ killcast.py
@@ -10,3 +10,10 @@ import requests
 import argparse
 import ipaddress
 from xml.etree import ElementTree
+requests.packages.urllib3.disable_warnings()
+
+R = '\033[31m' # red
+G = '\033[32m' # green
+C = '\033[36m' # cyan
+W = '\033[0m'  # white
+Y = '\033[33m' # yellow
