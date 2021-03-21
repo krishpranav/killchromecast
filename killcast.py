9b28e76b79b942a7520f59858ab76ce4e01e5624
krishpranav <krisna.pranav@gmail.com>
2021-03-21 17:57:57 +0530

    done chagnes

--- killcast.py
+++ killcast.py
@@ -17,3 +17,10 @@ G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m'  # white
 Y = '\033[33m' # yellow
+
+version = '1.0.3'
+
+parser = argparse.ArgumentParser(description="Manipulate Chromecast Devices is your Network")
+parser.add_arguments('-t', '--ip', help='IP Address of Chromecast', required=True)
+args = parser.parse_args()
+ip = args.ip
